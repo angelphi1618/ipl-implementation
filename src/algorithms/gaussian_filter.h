07688a279ae4e0b452af89e9aa0c43650e8e871c
Ángel Privado Almagro <angelphi1618@gmail.com>
2024-04-08 20:52:45 +0000

    added roi support for gaussian filter

--- src/algorithms/gaussian_filter.h
+++ src/algorithms/gaussian_filter.h
@@ -48,6 +48,7 @@ sycl::event gaussian_filter(sycl::queue& q, image<DataT, AllocatorT>& src, image
 
 		ComputeT sigma_x = kernel_spec.sigma_x;
 		ComputeT sigma_y = kernel_spec.sigma_y;
+		ComputeT normalization_factor = 1 / (2 * M_PI * sigma_x * sigma_y);
 
 		int k = kernel_width;
 
@@ -66,46 +67,74 @@ sycl::event gaussian_filter(sycl::queue& q, image<DataT, AllocatorT>& src, image
 			x_axis_component = x_axis_component / twice_sigma_x_sqrd;
 			y_axis_component = y_axis_component / twice_sigma_y_sqrd;
 
-			ComputeT val = exp(-(x_axis_component + y_axis_component));
+			ComputeT val = exp(-(x_axis_component + y_axis_component)) * normalization_factor;
 			gaussian_kernel[index] = val;
 
 		});
 	}).wait();
 
-	ComputeT* gaussian_kernel_host = (ComputeT*)malloc(kernel_height*kernel_width*sizeof(ComputeT));
-	q.memcpy(gaussian_kernel_host, gaussian_kernel, kernel_height*kernel_width*sizeof(ComputeT)).wait();
-
-	ComputeT suma = static_cast<ComputeT>(0);
-	for (int i = 0; i < kernel_height*kernel_width; i++)
-		{
-			suma += gaussian_kernel_host[i];
-			// std::cout << "suma = " << suma << std::endl;
-		}
-
-	// for (int i = 0; i < kernel_height; i++)
-	// {
-	// 	for (int j = 0; j < kernel_width; j++)
-	// 	{
-	// 		std::cout << gaussian_kernel_host[i*kernel_width + j] << "\t";
-	// 	}
-	// 	std::cout << std::endl;
-	// }
-	
-	// suma = (2 * 3.141592) * kernel_spec.sigma_x * kernel_spec.sigma_y;
-	for (int i = 0; i < kernel_height*kernel_width; i++)
-		gaussian_kernel_host[i] = gaussian_kernel_host[i] / (suma);
-
-	// for (int i = 0; i < kernel_height; i++)
-	// {
-	// 	for (int j = 0; j < kernel_width; j++)
-	// 	{
-	// 		std::cout << gaussian_kernel_host[i*kernel_width + j] << "\t";
-	// 	}
-	// 	std::cout << std::endl;
-	// }
-
 	// Generamos el kernel tal y como espera filter_convolution
-	filter_convolution_spec<ComputeT> kernel(sycl::range<2>(kernel_width, kernel_height), gaussian_kernel_host);
+	filter_convolution_spec<ComputeT> kernel(sycl::range<2>(kernel_width, kernel_height), gaussian_kernel);
 
 	return filter_convolution(q, src, dst, kernel, border_type, default_value, dependencies);
+}
+
+
+template <typename ComputeT = float, typename DataT, typename AllocatorT>
+sycl::event gaussian_filter_roi(sycl::queue& q, image<DataT, AllocatorT>& src, image<DataT, AllocatorT>& dst,
+						const gaussian_filter_spec<ComputeT>& kernel_spec,
+						border_types border_type = border_types::default_val,
+						pixel<DataT> default_value = {},
+						const std::vector<sycl::event>& dependencies = {})
+{
+    switch (border_type)
+	{	
+	case border_types::const_val:
+	case border_types::repl:
+    case border_types::mirror:
+		break;
+	default:
+		throw unimplemented("Tipo de borde no soportado");
+	}
+
+	// El kernel es cuadrado
+	int kernel_width = kernel_spec.kernel_size;
+	int kernel_height = kernel_spec.kernel_size;
+
+	ComputeT* gaussian_kernel = static_cast<ComputeT*>(src.get_allocator()->allocate_bytes(kernel_width*kernel_height*sizeof(ComputeT)));
+
+	// Inicializamos el kernel gaussiano
+	q.submit([&](sycl::handler& cgh) {
+
+		ComputeT sigma_x = kernel_spec.sigma_x;
+		ComputeT sigma_y = kernel_spec.sigma_y;
+		ComputeT normalization_factor = 1 / (2 * M_PI * sigma_x * sigma_y);
+
+		int k = kernel_width;
+
+		int twice_sigma_x_sqrd = 2 * sigma_x * sigma_x;
+		int twice_sigma_y_sqrd = 2 * sigma_y * sigma_y;
+
+		cgh.parallel_for(sycl::range<2>(kernel_height, kernel_width), [=](sycl::id<2> item){
+			int i = item.get(0);
+			int j = item.get(1);
+
+			int index = (i * kernel_width) + j;
+
+			ComputeT x_axis_component = (k/2 - i) * (k/2 - i);
+			ComputeT y_axis_component = (k/2 - j) * (k/2 - j);
+
+			x_axis_component = x_axis_component / twice_sigma_x_sqrd;
+			y_axis_component = y_axis_component / twice_sigma_y_sqrd;
+
+			ComputeT val = exp(-(x_axis_component + y_axis_component)) * normalization_factor;
+			gaussian_kernel[index] = val;
+
+		});
+	}).wait();
+
+	// Generamos el kernel tal y como espera filter_convolution
+	filter_convolution_spec<ComputeT> kernel(sycl::range<2>(kernel_width, kernel_height), gaussian_kernel);
+
+	return filter_convolution_roi(q, src, dst, kernel, border_type, default_value, dependencies);
 }
