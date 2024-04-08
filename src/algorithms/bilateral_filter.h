3b625d2c6cf2d221e0fcc175a4648262f1576d63
Ángel Privado Almagro <angelphi1618@gmail.com>
2024-04-08 22:39:21 +0000

    added roi support to bilateral filter

--- src/algorithms/bilateral_filter.h
+++ src/algorithms/bilateral_filter.h
@@ -20,7 +20,7 @@ struct bilateral_filter_spec{
 };
 
 template <typename ComputeT = float, typename DataT>
-inline ComputeT get_w(int i, int j, int k, int l, ComputeT twice_sigma_d_sqrd, ComputeT twice_sigma_i_sqrd, pixel<DataT>I_ij, pixel<DataT> I_kl, sycl::stream os){
+inline ComputeT get_w(int i, int j, int k, int l, ComputeT twice_sigma_d_sqrd, ComputeT twice_sigma_i_sqrd, pixel<DataT>I_ij, pixel<DataT> I_kl){
 	
 	// L1 norm
 	ComputeT first_term = (ComputeT) ((i - k)*(i - k) + (j - l)*(j - l));
@@ -75,7 +75,7 @@ sycl::event bilateral_filter(sycl::queue& q, image<DataT, AllocatorT>& src, imag
 
 		int dst_width = dst.get_size().get(0);
 
-		sycl::stream os(1024*1024, 1024, cgh);
+		// sycl::stream os(1024*1024, 1024, cgh);
 
 		std::cout << "lanzando parallel for" << std::endl;
 		// Tamaño de la imagen destino
@@ -113,7 +113,7 @@ sycl::event bilateral_filter(sycl::queue& q, image<DataT, AllocatorT>& src, imag
 
 					pixel<DataT> I_kl = src_data[kk_src_bordered * src_bordered_width + (ll_src_bordered)];
 
-					ComputeT w = get_w(i_src_bordered, j_src_bordered, kk_src_bordered, ll_src_bordered, twice_sigma_d_sqrd, twice_sigma_i_sqrd, I_ij, I_kl, os);
+					ComputeT w = get_w(i_src_bordered, j_src_bordered, kk_src_bordered, ll_src_bordered, twice_sigma_d_sqrd, twice_sigma_i_sqrd, I_ij, I_kl);
 
 					if (w > 0.0){
 						//os << "NUMERO W = 0" << sycl::endl;
@@ -134,7 +134,123 @@ sycl::event bilateral_filter(sycl::queue& q, image<DataT, AllocatorT>& src, imag
 				}
 			}
 			
-			os << (int) sum_Iw_R << " / " << (int) sum_w << " = " << (int)(sum_Iw_R / sum_w) << sycl::endl;
+			// os << (int) sum_Iw_R << " / " << (int) sum_w << " = " << (int)(sum_Iw_R / sum_w) << sycl::endl;
+
+			//os << "R=" << (int) (sum_Iw_R / sum_w) << sycl::endl;
+
+			dst_data[i_destino * dst_width + j_destino] = {
+				(DataT) (sum_Iw_R / sum_w),
+				(DataT) (sum_Iw_G / sum_w),
+				(DataT) (sum_Iw_B / sum_w),
+				(DataT) (sum_Iw_A / sum_w),
+			};
+
+			// os << "sumaR = " << suma.R << ", sumaG = " << suma.G << ", sumaB = " << suma.B << ", sumaA = " << suma.A << sycl::endl;
+		});
+	});
+}
+
+template <typename ComputeT = float,
+		typename DataT, typename AllocatorT>
+sycl::event bilateral_filter_roi(sycl::queue& q, image<DataT, AllocatorT>& src, image<DataT, AllocatorT>& dst,
+						const bilateral_filter_spec<ComputeT>& spec,
+						border_types border_type = border_types::default_val,
+						pixel<DataT> default_value = {},
+						const std::vector<sycl::event>& dependencies = {}) {
+
+	switch (border_type)
+	{	
+	case border_types::const_val:
+	case border_types::repl:
+		break;
+
+	default:
+		throw unimplemented("Tipo de borde no soportado");
+	}
+	
+	int kernel_height = spec.kernel_size;
+	int kernel_width = spec.kernel_size;
+
+	image<DataT, AllocatorT>* bordered_image = generate_border(src, sycl::range<2>(kernel_width, kernel_height), border_type, default_value);
+	
+
+	return q.submit([&](sycl::handler& cgh) {
+
+		cgh.depends_on(dependencies);
+
+		std::cout << "kernel copiado" << std::endl;
+
+		//std::cout << typeid(*src.get_allocator()).name() << std::endl;
+
+		pixel<DataT>* src_data = bordered_image->get_data();
+		pixel<DataT>* dst_data = dst.get_data();
+
+		int src_bordered_width = bordered_image->get_size().get(0);
+
+		int dst_width = dst.get_size().get(0);
+
+		// sycl::stream os(1024*1024, 1024, cgh);
+
+		std::cout << "lanzando parallel for" << std::endl;
+		// Tamaño de la imagen destino
+		int x_anchor = (kernel_width - 1) / 2;
+		int y_anchor = (kernel_height - 1) / 2;
+
+		ComputeT twice_sigma_d_sqrd = 2 * spec.sigma_distance * spec.sigma_distance;
+		ComputeT twice_sigma_i_sqrd = 2 * spec.sigma_intensity * spec.sigma_intensity;
+
+		roi_rect roi = src.get_roi_rect();
+
+		int src_bordered_height = bordered_image->get_size().get(1);
+
+		q.memcpy(dst_data, src.get_data(), src.get_linear_size() * sizeof(pixel<DataT>)).wait();
+
+		cgh.parallel_for(sycl::range<2>(roi.get_height(), roi.get_width()), [=](sycl::id<2> item){
+			// os << "dentro del kernel bilateral" << sycl::endl;
+
+			// os << "kernel usado" << sycl::endl;
+
+			int i_destino = item.get(1) + roi.get_x_offset();
+			int j_destino = item.get(0) + (src_bordered_height - roi.get_y_offset() - roi.get_height());
+
+			int i_src_bordered = i_destino + kernel_height;
+			int j_src_bordered = j_destino + kernel_width;
+
+			pixel<DataT> I_ij = src_data[i_src_bordered * src_bordered_width + j_src_bordered];
+
+			ComputeT sum_w = 0;
+			ComputeT sum_Iw_R = 0;
+			ComputeT sum_Iw_G = 0;
+			ComputeT sum_Iw_B = 0;
+			ComputeT sum_Iw_A = 0;
+
+			for (int k = 0; k < kernel_height; k++)
+			{
+				for (int l = 0; l < kernel_width; l++)
+				{
+					int kk_src_bordered = k + i_src_bordered - y_anchor;
+					int ll_src_bordered = l + j_src_bordered - x_anchor;
+
+					pixel<DataT> I_kl = src_data[kk_src_bordered * src_bordered_width + (ll_src_bordered)];
+
+					ComputeT w = get_w(i_src_bordered, j_src_bordered, kk_src_bordered, ll_src_bordered, twice_sigma_d_sqrd, twice_sigma_i_sqrd, I_ij, I_kl);
+
+					sum_w += w;
+
+					sum_Iw_R += I_kl.R * w;
+					sum_Iw_G += I_kl.G * w;
+					sum_Iw_B += I_kl.B * w;
+					sum_Iw_A += I_kl.A * w;
+
+
+					//R = R + ((ComputeT)src_data[kk_src_bordered * src_bordered_width + (ll_src_bordered)].R * kernel_data[k * kernel_width + l]);
+					//G = G + ((ComputeT)src_data[kk_src_bordered * src_bordered_width + (ll_src_bordered)].G * kernel_data[k * kernel_width + l]);
+					//B = B + ((ComputeT)src_data[kk_src_bordered * src_bordered_width + (ll_src_bordered)].B * kernel_data[k * kernel_width + l]);
+					//A = A + ((ComputeT)src_data[kk_src_bordered * src_bordered_width + (ll_src_bordered)].A * kernel_data[k * kernel_width + l]);
+				}
+			}
+			
+			// os << (int) sum_Iw_R << " / " << (int) sum_w << " = " << (int)(sum_Iw_R / sum_w) << sycl::endl;
 
 			//os << "R=" << (int) (sum_Iw_R / sum_w) << sycl::endl;
 
