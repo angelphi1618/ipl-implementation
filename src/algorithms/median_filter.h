5aecf61bf5e581b5e5ac521a062c4f75618c1730
Ángel Privado Almagro <angelphi1618@gmail.com>
2024-04-09 11:44:16 +0000

    added roi support for median_filter

--- src/algorithms/median_filter.h
+++ src/algorithms/median_filter.h
@@ -96,7 +96,76 @@ sycl::event median_filter(sycl::queue& q, image<DataT, AllocatorT>& src, image<D
 }
 
 
+template <typename DataT, typename AllocatorT>
+sycl::event median_filter_roi(sycl::queue& q, image<DataT, AllocatorT>& src, image<DataT, AllocatorT>& dst,
+						const median_spec& spec,
+						border_types border_type = border_types::repl,
+						pixel<DataT> default_value = {},
+						const std::vector<sycl::event>& dependencies = {}) {
+
+	switch (border_type)
+	{	
+	case border_types::const_val:
+	case border_types::repl:
+		break;
+	
+	default:
+		throw unimplemented("Tipo de no soportado");
+	}
+	
+	if (spec.window_size > MAX_WINDOW)
+		throw invalid_argument("Tamaño de ventana no permitido. Máx. 5.");
+
+	image<DataT, AllocatorT>* bordered_image = generate_border<DataT, AllocatorT>(src, sycl::range<2>(spec.window_size, spec.window_size), border_type, default_value);
 
 
+	return q.submit([&](sycl::handler& cgh) {
 
+		cgh.depends_on(dependencies);
+		pixel<DataT>* src_data = bordered_image->get_data();
+		pixel<DataT>* dst_data = dst.get_data();
 
+		int src_bordered_width = bordered_image->get_size().get(0);
+		int dst_width = dst.get_size().get(0);
+
+        int anchor = (spec.window_size - 1) / 2;
+		
+		roi_rect roi = src.get_roi_rect();
+		
+		int src_bordered_height = bordered_image->get_size().get(1);
+		
+		q.memcpy(dst_data, src.get_data(), src.get_linear_size() * sizeof(pixel<DataT>)).wait();
+
+		cgh.parallel_for(sycl::range<2>(roi.get_height(), roi.get_width()), [=](sycl::id<2> item){
+            pixel<DataT> window[MAX_WINDOW * MAX_WINDOW];
+
+			int i_destino = item.get(1) + roi.get_x_offset();
+			int j_destino = item.get(0) + (src_bordered_height - roi.get_y_offset() - roi.get_height());
+
+			int i_src_bordered = i_destino + spec.window_size;
+			int j_src_bordered = j_destino + spec.window_size;
+
+			for (int ii = 0; ii < spec.window_size; ii++)
+			{
+				for (int jj = 0; jj < spec.window_size; jj++)
+				{
+					int ii_src_bordered = ii + i_src_bordered - anchor;
+					int jj_src_bordered = jj + j_src_bordered - anchor;
+
+					window[ii * spec.window_size + jj] = src_data[ii_src_bordered * src_bordered_width + jj_src_bordered];
+				}
+			}
+
+            buble_sort(window, spec.window_size*spec.window_size);
+
+			pixel<DataT> currentPixel = src_data[(i_src_bordered) * src_bordered_width + (j_src_bordered)];
+			pixel<DataT> median = window[(spec.window_size * spec.window_size - 1)/2];
+
+			double medianValue = median.value();
+			double pixelValue =  currentPixel.value();
+
+			dst_data[i_destino * dst_width + j_destino] = median;
+		});
+	});
+
+}
